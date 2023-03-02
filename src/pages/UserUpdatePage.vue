62db21ced167caace02acb8bf6f84695f2ab46ae
hongxiaobin <2812181610@qq.com>
2023-03-02 20:07:15 +0800

    feat:
    开发前端页面

--- src/pages/UserUpdatePage.vue
+++ src/pages/UserUpdatePage.vue
@@ -8,7 +8,7 @@
     <van-cell title="性别" is-link :value="user.gender" @click="toEdit('gender', '性别', user.gender)"/>
     <van-cell title="电话" is-link to="/user/edit" :value="user.phone" @click="toEdit('phone', '电话', user.phone)"/>
     <van-cell title="邮箱" is-link to="/user/edit" :value="user.email" @click="toEdit('email', '邮箱', user.email)"/>
-    <van-cell title="星球编号" :value="user.planetCode"/>
+    <van-cell title="用户编号" :value="user.planetCode"/>
     <van-cell title="注册时间" :value="user.createTime"/>
   </template>
 </template>
@@ -22,7 +22,7 @@ import {getCurrentUser} from "../services/user";
 
 // const user = {
 //   id: 1,
-//   username: '鱼皮',
+//   username: 'xiobin',
 //   userAccount: 'dogYupi',
 //   avatarUrl: 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/img/logo.png',
 //   gender: '男',
