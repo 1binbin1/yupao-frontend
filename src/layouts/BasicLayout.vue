fc79792298b09bc96a94f28d9901ec74b481b0cc
hongxiaobin <2812181610@qq.com>
2023-03-02 18:26:49 +0800

    feat:
    使用vue-router整合页面路由

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -14,41 +14,24 @@
 
 	<!--	引入不同的页面-->
 	<div id="content">
-		<template v-if="active === 'index'">
-			<Index/>
-		</template>
-		<template v-if="active === 'team'">
-			<Team/>
-		</template>
-		<template v-if="active === 'user'">
-			<User/>
-		</template>
+		<router-view/>
 	</div>
 
 	<!-- 底部tab-->
-	<van-tabbar v-model="active" @change="onChange">
-		<van-tabbar-item icon="home-o" name="index">主页</van-tabbar-item>
-		<van-tabbar-item icon="search" name="team">队伍页</van-tabbar-item>
-		<van-tabbar-item icon="friends-o" name="user">个人</van-tabbar-item>
+	<van-tabbar route @change="onChange">
+		<van-tabbar-item to="/" icon="home-o" name="index">主页</van-tabbar-item>
+		<van-tabbar-item to="/team" icon="search" name="team">队伍页</van-tabbar-item>
+		<van-tabbar-item to="/user" icon="friends-o" name="user">个人</van-tabbar-item>
 	</van-tabbar>
 
 </template>
 
 <!-- 使用setup语法糖，将数据暴露给页面而不需要进行写return-->
 <script setup>
-	import {ref} from 'vue';
 	import {Toast} from 'vant';
-	import Index from "../pages/Index.vue";
-	import Team from "../pages/Team.vue";
-	import User from "../pages/User.vue";
 
-	const onClickLeft = () => alert("左");
-	const onClickRight = () => alert("右");
-
-	// 默认高亮哪一个页面，使用标签的name值
-	const active = ref("index");
 	// 更换页面时轻量的提示
-	const onChange = (index) => Toast(`标签 ${index}`);
+	// const onChange = (index) => Toast(`标签 ${index}`);
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import {createApp} from 'vue'
 import App from './App.vue'
 
 // 要引入的组件
-import {Button, NavBar, Icon,Tabbar, TabbarItem } from 'vant';
+import {Button, NavBar, Icon, Tabbar, TabbarItem} from 'vant';
+import * as VueRouter from 'vue-router';
+import routes from "./config/route";
 
 
 const app = createApp(App);
@@ -13,4 +15,16 @@ app.use(Icon);
 app.use(Tabbar);
 app.use(TabbarItem);
 
+
+const router = VueRouter.createRouter({
+    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    history: VueRouter.createWebHashHistory(),
+    routes, // `routes: routes` 的缩写
+})
+app.use(router);
+
 app.mount('#app')
+
+
+
+
