3d1d0863824bb302c54f5e44c6aea559ee50d1fd
hongxiaobin <2812181610@qq.com>
2023-03-02 18:52:22 +0800

    feat:
    开发前端页面

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-
 import BasicLayout from "./layouts/BasicLayout.vue";
 </script>
 
 <template>
-<!--	引入页面-->
-	<BasicLayout/>
+  <BasicLayout />
 </template>
 
-<style scoped>
+<style>
 
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,19 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-
-// 要引入的组件
-import {Button, NavBar, Icon, Tabbar, TabbarItem} from 'vant';
 import * as VueRouter from 'vue-router';
 import routes from "./config/route";
-
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import '../global.css'
 
 const app = createApp(App);
-// 注册使用
-app.use(Button);
-app.use(NavBar);
-app.use(Icon);
-app.use(Tabbar);
-app.use(TabbarItem);
-
+app.use(Vant);
 
 const router = VueRouter.createRouter({
-    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-    history: VueRouter.createWebHashHistory(),
+    // 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    history: VueRouter.createWebHistory(),
     routes, // `routes: routes` 的缩写
 })
+
 app.use(router);
-
 app.mount('#app')
-
-
-
-
